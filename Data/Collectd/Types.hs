c4725da686021779ea3a8509267e3c8b075edf31
Kim Altintop <kim.altintop@gmail.com>
2014-04-28 11:21:21 +0200

    Add (convenience) getters for Notification

--- Data/Collectd/Types.hs
+++ Data/Collectd/Types.hs
@@ -79,3 +79,44 @@ data Severity
     | Warning
     | Okay
     deriving (Eq, Show)
+
+
+notifSeverity :: Notification -> Severity
+notifSeverity (Notification hdrs _) = head [ s | Severity s <- hdrs ]
+
+notifTime :: Notification -> Double
+notifTime (Notification hdrs _) = head [ t | Time t <- hdrs ]
+
+notifMessage :: Notification -> Message
+notifMessage (Notification _ m) = m
+
+notifHost :: Notification -> Maybe Host
+notifHost (Notification hdrs _) = case [ h | Host h <- hdrs ] of
+    []    -> Nothing
+    (h:_) -> Just h
+
+notifPlugin :: Notification -> Maybe Plugin
+notifPlugin (Notification hdrs _) = case [ p | Plugin p <- hdrs ] of
+    []    -> Nothing
+    (p:_) -> Just p
+
+notifPluginInst :: Notification -> Maybe PluginInst
+notifPluginInst (Notification hdrs _) = case [ p | PluginInstance p <- hdrs ] of
+    []    -> Nothing
+    (p:_) -> Just p
+
+notifTyp :: Notification -> Maybe Typ
+notifTyp (Notification hdrs _) = case [ t | Type t <- hdrs ] of
+    []    -> Nothing
+    (t:_) -> Just t
+
+notifTypInst :: Notification -> Maybe TypInst
+notifTypInst (Notification hdrs _) = case [ t | TypeInstance t <- hdrs ] of
+    []    -> Nothing
+    (t:_) -> Just t
+
+notifOtherHeaders :: Notification -> [(Text,Text)]
+notifOtherHeaders (Notification hdrs _) = [ (k,v) | UnknownHeader k v <- hdrs ]
+
+notifLookupHeader :: Notification -> Text -> Maybe Text
+notifLookupHeader notif name = lookup name (notifOtherHeaders notif)
